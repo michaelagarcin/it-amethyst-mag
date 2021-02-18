b3ac45c9b0d04a642524c32aaedc9b1c01baeeba
michaelagarcin <michaelagarcin@gmail.com>
2021-02-18 17:01:57 +0000

    new commit

---
+++ dragon/js/_vti_cnf/jquery.js
@@ -0,0 +1,10 @@
+vti_encoding:SR|utf8-nl
+vti_timelastmodified:TR|04 Dec 2019 20:42:50 -0000
+vti_extenderversion:SR|12.0.0.0
+vti_author:SR|Michael\\Michael Garcin
+vti_modifiedby:SR|Michael\\Michael Garcin
+vti_timecreated:TR|04 Dec 2019 20:39:51 -0000
+vti_backlinkinfo:VX|main.dwt main.html
+vti_cacheddtm:TX|04 Dec 2019 20:42:50 -0000
+vti_filesize:IR|88145
+vti_nexttolasttimemodified:TR|04 Dec 2019 20:39:51 -0000
---
+++ dragon/php/_vti_cnf/mail-form.php
@@ -0,0 +1,15 @@
+vti_encoding:SR|utf8-nl
+vti_timelastmodified:TR|05 Dec 2019 04:44:11 -0000
+vti_extenderversion:SR|12.0.0.0
+vti_backlinkinfo:VX|contact.html
+vti_author:SR|Michael\\Michael Garcin
+vti_modifiedby:SR|Michael\\Michael Garcin
+vti_timecreated:TR|05 Dec 2019 04:35:34 -0000
+vti_nexttolasttimemodified:TW|05 Dec 2019 04:41:33 -0000
+vti_cacheddtm:TX|05 Dec 2019 04:44:11 -0000
+vti_filesize:IR|2699
+vti_cachedneedsrewrite:BR|false
+vti_cachedhasbots:BR|false
+vti_cachedhastheme:BR|false
+vti_cachedhasborder:BR|false
+vti_charset:SR|utf-8
